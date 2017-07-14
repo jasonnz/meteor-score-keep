13488a47c483544e4eed8994cf82846f76102ee5
Jason Marshall <jasonnz@gmail.com>
2017-07-14 20:44:26 +1200

    further styling and finishing up of project

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -4,15 +4,17 @@ import {Players} from './../api/players';
 
 export default class Player extends React.Component {
     render() {
-         return (
-            <div key={this.props.player._id} className='item'>
+        let itemClassName = `item item--position-${this.props.player.rank}`;
+        console.log(itemClassName);
+        return (
+            
+            <div key={this.props.player._id} className={itemClassName}>
                 <div className='player'>
                     <div>
                         <h3 className='player__name'>{this.props.player.name}</h3>
                         <p  className='player__stats'>
+                            {this.props.player.position} place - 
                             has {this.props.player.score} point(s).
-                            {this.props.player.rank}
-                            {this.props.player.position}
                         </p>
                     </div>
                     <div className='player__actions'>
